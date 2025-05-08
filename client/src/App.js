32685c6943433e5fa6d787b9fa52d6097203f686
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2025-05-07 22:24:43 -0400

    worked on setting up Apollo Client with InMemoryCache and HTTP link for GraphQL integration

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';     //utility from apollo client which lets modify the request context, when sending a request/ recieving a response.
+//import necessary react and apollo packages\
+
+import { StoreProvider } from './utils/globalState';
+
+const httpLink = createHttpLink({
+    uri: '/graphql',     // the uri specifies the graphql server's endpoint - in this case, "/graphql". when a "mutation" or "query" occurs, the request will be sent to '/graphql
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+})
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+
+
+            </Router>
+        </ApolloProvider>
+    )
+}
+
+export default App;

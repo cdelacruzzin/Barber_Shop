dcb9b54bacb7c31cbb27073047a4af1e1737421e
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2025-05-11 04:34:54 -0400

    finished menu drawer items

--- client/src/components/Navbar/HamburgerMenu/index.js
+++ client/src/components/Navbar/HamburgerMenu/index.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import {ListItemIcon, ListSubheader, List, ListItem, IconButton} from "@mui/material";
+import {ListItemIcon, ListSubheader, List, ListItem, IconButton, ListItemButton, ListItemText} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import ToggleButton from "@mui/material/ToggleButton";
+import { useLocation, Link, useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
 
 const Menu = ({ onClick, onKeyDown }) => {
     return (
+        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
         <List>
             <ListItem >
                 <IconButton onClick={onClick} onKeyDown={onKeyDown} button sx={{ border: '1px solid', borderColor: 'divider', borderRadius: 1.5, display: 'flex',
@@ -14,8 +15,33 @@ const Menu = ({ onClick, onKeyDown }) => {
                     <MenuIcon />
                 </IconButton>
             </ListItem>
+            <ListItem >
+                <ListItemButton component={Link} to={"/home"} sx={{borderBottom: '1px solid', borderColor: 'divider', borderRadius: 1, display: 'flex',}}>
+                    <ListItemText primary="HOME" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem >
+                <ListItemButton component={Link} to={"/collections/all"}  sx={{borderBottom: '1px solid', borderColor: 'divider', borderRadius: 1, display: 'flex',}}>
+                    <ListItemText primary="ALL PRODUCTS" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem >
+                <ListItemButton component={Link} to={"/collections"}  sx={{borderBottom: '1px solid', borderColor: 'divider', borderRadius: 1, display: 'flex',}}>
+                    <ListItemText primary="COLLECTIONS" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem >
+                <ListItemButton component={Link} to={"/pages/contact-us"}  sx={{borderBottom: '1px solid', borderColor: 'divider', borderRadius: 1, display: 'flex',}}>
+                    <ListItemText primary="CONTACT" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem >
+                <ListItemButton component={Link} to={"/pages/faq"}  sx={{borderBottom: '1px solid', borderColor: 'divider', borderRadius: 1, display: 'flex',}}>
+                    <ListItemText primary="FAQ" />
+                </ListItemButton>
+            </ListItem>
         </List>
-
+        </Box>
     );
 };
 

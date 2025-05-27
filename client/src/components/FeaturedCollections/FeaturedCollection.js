31611ebb1d357fca4a8dab38335094c5182650ee
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2025-05-26 22:54:47 -0400

    fixed hamburger menu and cart menu to be responsive

--- client/src/components/FeaturedCollections/FeaturedCollection.js
+++ client/src/components/FeaturedCollections/FeaturedCollection.js
@@ -3,23 +3,42 @@ import {Link} from 'react-router-dom';
 import {Box} from "@mui/system";
 import Flickity from 'flickity';
 import carousel from '../../styles/FeaturedCollections.css'
-import {Tab, Tabs} from '@mui/material';
+import {Tab, Tabs, Typography} from '@mui/material';
 
 
 export default function FeaturedCollection(props) {
     // const coffeeProduct = require.context('../assets/featured/collection/coffee');
     // const coffeeProductURLs = coffeeProduct.keys().map(coffeeProduct);
 
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        console.log(value);
+    };
+
     return (
         <div style={{}}>
             <section id='section'>
-                <header style={{height:'10vh', backgroundColor: 'white'}}>
-                    <h4> Featured Collection</h4>
-                    <Tabs>
-                        <Tab></Tab>
-                        <Tab></Tab>
+                <header style={{height:'100%', display:'flex',flexDirection:'column', justifyContent:'center', marginTop: 4, backgroundColor:'white' }}>
+                    <h5 style ={{paddingTop: '2%', textAlign:'center'}}> Featured Collection</h5>
+                    {/*<Tabs value={activeTab} onchange={changeTab}>*/}
+                    {/*    <Tab label={"coffee"}/>*/}
+                    {/*    <Tab label={"standee"}/>*/}
+                    {/*</Tabs>*/}
+                    <Box sx={{ width: '100%', backgroundColor: 'yellow', display:'flex', flexDirection:'column', justifyContent:'center', alignItems: 'center' }}>
+                        {/*<div style={{backgroundColor:'red', height:'100%',display:'flex', justifyContent:'center', alignItems: 'center'}}>*/}
+                        {/*    <Typography sx={{px:5}}>Custom Roast Coffee</Typography>*/}
+                        {/*    <Typography sx={{px:5}}>Custom Roast Coffee</Typography>*/}
+                        {/*</div>*/}
+                        <Tabs root={{}} value={value} onChange={handleChange} centered>
+                            <Tab label="Custom Roeast Coffee" />
+                            <Tab label="alternative outfit standee" />
+                        </Tabs>
+                    </Box>
 
-                    </Tabs>
+
+
                 </header>
                 <div role={"tab panel"}>
                     <div className={"ProductListWrapper"}>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -9,10 +9,11 @@ import CartMenu from "./CartMenu";
 
 
 
-import Drawer from '@mui/material/Drawer';
+import {Drawer, Grid} from '@mui/material';
 import Menu from "./HamburgerMenu";
 import {Button, ListItemButton, ListItemText,} from "@mui/material";
 import logo from "../../assets/logo.png";
+
 
 
 const Navbar = () => {
@@ -31,8 +32,8 @@ const Navbar = () => {
     const menu = (anchor) => {
         const handleClose = toggleDrawer(anchor, false);
         return (
-            <Box
-                sx={{width: '18vw', display: 'flex'}} >
+            <Box>
+                {/*sx={{width: '18vw', display: 'flex'}}*/}
                 <Menu onClick={handleClose} onKeyDown={handleClose} />
             </Box>
         );
@@ -41,13 +42,7 @@ const Navbar = () => {
     const cart = (anchor) => {
         const handleClose = toggleDrawer(anchor, false);
         return (
-            <Box
-                sx={{
-                width: '400',
-                display: 'flex',
-                justifyContent: 'space-between',
-                    backgroundColor:'yellow'
-            }}>
+            <Box>
                 <CartMenu onClick={handleClose} onKeyDown={handleClose} />
             </Box>
 
@@ -57,31 +52,72 @@ const Navbar = () => {
         <>
 
             <Box
-                sx={{  backgroundColor: 'black', display: 'grid',  placeItems: 'center',  px: 2, color:'white', py: 1 }} >
+                sx={{
+                    backgroundColor: "black",
+                    display: "grid",
+                    placeItems: "center",
+                    px: { xs: 2, sm: 4 },
+                    py: { xs: 1, sm: 1.5 },
+                    color: "white",
+                    fontSize: { xs: "0.8rem", sm: "1rem" },
+                }}>
                 <Link to={"/all-products"} style ={{textDecoration:'none', color: 'inherit'}}>
                     pre-order your favourite items! Free shipping for orders over $150
                 </Link>
             </Box>
 
 
-            <Box sx={{ position: 'sticky',   top: 0,  zIndex: 2,  backgroundColor: 'black',  height: '8vh', display: 'flex',  alignItems: 'center', justifyContent: 'space-between', px: 0  }}>
-                <HamburgerBtn anchor="left" onClick={toggleDrawer("left", true)}>
-                    <Drawer  anchor={"left"}  open={state["left"]} onClose={toggleDrawer("left", false)}>
-                        {menu("left")}
-                    </Drawer>
-                </HamburgerBtn>
-                <Box component={Link} to={"/"}>
-                    <img src={logo} alt={logo} />
-                </Box>
-                <CartBtn anchor="right" onClick={toggleDrawer("right", true)}>
-                    <Drawer
-                        anchor={"right"}
-                        open={state["right"]}
-                        onClose={toggleDrawer("right", false)}>
-                        {cart("right")}
-                    </Drawer>
-                </CartBtn>
-            </Box>
+            <Grid
+                container
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{
+                    position: "sticky",
+                    top: 0,
+                    zIndex: 2,
+                    backgroundColor: "black",
+                    height: { xs: "8vh", sm: "10vh" },
+                    // px: { xs: "2vw", sm: "4vw" },
+                }}>
+                <Grid>
+                    <HamburgerBtn anchor="left" onClick={toggleDrawer("left", true)}>
+                        <Drawer
+                            anchor="left"
+                            open={state["left"]}
+                            onClose={toggleDrawer("left", false)}
+                            PaperProps={{
+                                sx: {
+                                    width: { xs: "70vw", sm: "30vw", md: "17vw" },
+                                },
+                            }}
+                        >
+                            {menu("left")}
+                        </Drawer>
+                    </HamburgerBtn>
+                </Grid>
+                <Grid sx={{display: "flex", justifyContent: "center"}}>
+                    <Box component={Link} to={"/"} sx={{display: 'inline-block', maxWidth: '40vw'}}>
+                        <img src={logo} alt={logo} />
+                    </Box>
+                </Grid>
+
+                <Grid>
+                    <CartBtn anchor="right" onClick={toggleDrawer("right", true)}>
+                        <Drawer
+                            anchor="right"
+                            open={state["right"]}
+                            onClose={toggleDrawer("right", false)}
+                            PaperProps={{
+                                sx: {
+                                    width: { xs: "70vw", sm: "30vw", md: "21vw" },
+                                },
+                            }}>
+                            {cart("right")}
+                        </Drawer>
+                    </CartBtn>
+                </Grid>
+
+            </Grid>
         </>
     )
 }
